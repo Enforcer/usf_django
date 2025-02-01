355c320a18ecf4f323ee2c730f3724c7abf1ad3d
Sebastian Buczyński <nnplaya@gmail.com>
2025-02-01 16:36:54 +0100

    Implement ProductAvailability with tests

--- tests/products/test_product_availability.py
+++ tests/products/test_product_availability.py
@@ -1,8 +1,55 @@
-import pytest
+from datetime import datetime, timedelta
 
-from products.product_availability import ProductAvailability
+import pytest
+from products.product_availability import ProductAlreadyReserved, ProductAvailability
+from time_machine import travel
 
 
 @pytest.fixture()
 def product_availability() -> ProductAvailability:
     return ProductAvailability(product_id=1)
+
+
+def test_cannot_reserve_for_another_user_when_reservation_was_made_for_other_person(
+    product_availability: ProductAvailability,
+) -> None:
+    product_availability.reserve(for_=1, until=datetime.now() + timedelta(hours=1))
+
+    with pytest.raises(ProductAlreadyReserved):
+        product_availability.reserve(for_=2, until=datetime.now() + timedelta(hours=1))
+
+
+def test_can_reserve_for_the_same_user(
+    product_availability: ProductAvailability,
+) -> None:
+    product_availability.reserve(for_=1, until=datetime.now() + timedelta(hours=1))
+
+    try:
+        product_availability.reserve(for_=1, until=datetime.now() + timedelta(hours=1))
+    except ProductAlreadyReserved:
+        pytest.fail("Reservation for the same person should be possible!")
+
+
+def test_can_reserve_for_another_person_if_previous_reservation_expired(
+    product_availability: ProductAvailability,
+) -> None:
+    now = datetime.now()
+    product_availability.reserve(for_=1, until=now + timedelta(hours=1))
+
+    with travel(now + timedelta(hours=2)):
+        try:
+            product_availability.reserve(for_=2, until=now + timedelta(hours=1))
+        except ProductAlreadyReserved:
+            pytest.fail("Reservation after expiration should be possible!")
+
+
+def test_freed_product_can_be_reserved_again(
+    product_availability: ProductAvailability,
+) -> None:
+    product_availability.reserve(for_=1, until=datetime.now() + timedelta(days=2))
+    product_availability.free()
+
+    try:
+        product_availability.reserve(for_=2, until=datetime.now() + timedelta(days=2))
+    except ProductAlreadyReserved:
+        pytest.fail("Freed product should've been available for reservation")
--- usf/products/product_availability.py
+++ usf/products/product_availability.py
@@ -1,2 +1,38 @@
-class ProductAvailability:
+from datetime import datetime
+
+type ProductId = int
+type UserId = int
+
+
+class ProductAlreadyReserved(Exception):
     pass
+
+
+class ProductAvailability:
+    def __init__(
+        self,
+        product_id: ProductId,
+        reserved_for: UserId | None = None,
+        reserved_until: datetime | None = None,
+    ) -> None:
+        self._product_id = product_id
+        self._reserved_for = reserved_for
+        self._reserved_until = reserved_until
+
+    def reserve(self, for_: UserId, until: datetime) -> None:
+        if self._is_free() or self._reserved_for == for_:
+            self._reserved_for = for_
+            self._reserved_until = until
+        else:
+            raise ProductAlreadyReserved
+
+    def _is_free(self) -> bool:
+        return (
+            self._reserved_for is None
+            or self._reserved_until is None
+            or self._reserved_until <= datetime.now()
+        )
+
+    def free(self) -> None:
+        self._reserved_for = None
+        self._reserved_until = None
